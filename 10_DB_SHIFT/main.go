66a0820228af3675ef6309311187c5540a1f5880
Nishan8583 <nishan@rigotechnology.com>
2019-07-02 14:45:49 +0545

    interface usage

--- 10_DB_SHIFT/main.go
+++ 10_DB_SHIFT/main.go
@@ -2,14 +2,15 @@ package main
 
 // s
 import (
-        "database/sql"
-        "fmt"
-        //"errors"
-        _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+
+	//"errors"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func checkErr(err error) {
-        fmt.Println(err)
+	fmt.Println(err)
 }
 
 /* CREATE DATABASE testdb;
@@ -29,51 +30,65 @@ Query OK, 1 row affected (0.06 sec)
 
 mysql> INSERT INTO userinfo (username,departname,age) VALUES ("lolta","R&D",50);
 Query OK, 1 row affected (0.00 sec)
+stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,age=?")
+checkErr(err)
 
+// Actually executing the statement
+res, err := stmt.Exec("newguy", "SOC", 80)
+checkErr(err)
+
+id, err := res.LastInsertId()
+checkErr(err)
+fmt.Println(id)
 */
 
 // Function that connects to db and does stuff
 func ConnectDB() {
 
-        // registering the database driver interface, now the function from mysql driver will work
-        db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/testdb?charset=utf8")
-        if err != nil {
-                fmt.Println("An error occured while trying to connect to database", err)
-                return
-        }
-        defer db.Close()
-        fmt.Println("connection was successfull")
+	// registering the database driver interface, now the function from mysql driver will work
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/testdb?charset=utf8")
+	if err != nil {
+		fmt.Println("An error occured while trying to connect to database", err)
+		return
+	}
+	defer db.Close()
+	fmt.Println("connection was successfull")
 
-        // Statement to be executed is being prepared, ? is the placeholder
-        stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,age=?")
-        checkErr(err)
+	// Statement to be executed is being prepared, ? is the placeholder
 
-  // Actually executing the statement
-        res, err := stmt.Exec("astaxie", "SOC", 80)
-        checkErr(err)
+	// Read everything from the table
+	rows, err := db.Query(`SELECT * FROM userinfo`)
+	checkErr(err)
+	defer rows.Close()
 
-        id, err := res.LastInsertId()
-        checkErr(err)
-        fmt.Println(id)
+	cols, err := rows.Columns()            // Remember to check err afterwards
+	vals := make([]interface{}, len(cols)) // Making a slice of any type
+	for i, _ := range cols {
+		vals[i] = new(sql.RawBytes) // Each element
+	}
 
-  // Read everything from the table
-        rows, err := db.Query(`SELECT * FROM userinfo`)
-        checkErr(err)
-        defer rows.Close();
+	for rows.Next() { // next() prepares the next result that will be used by the scan
+		err = rows.Scan(vals...)
+		fmt.Println(err, vals)
+		for _, v := range vals {
+			switch s := v.(type) {
+			case *sql.RawBytes:
+				_ = s
+				fmt.Println(string(*v.(*sql.RawBytes)))
+			}
+		}
+		/*var username string
+		  var department string
+		  var age int
+		  err = rows.Scan(&username, &department, &age)  // Actually read form the dataset
+		  checkErr(err)
+		  fmt.Println(username)
+		  fmt.Println(department)
+		  fmt.Println(age)*/
+	}
 
-
-        for rows.Next() {  // next() prepares the next result that will be used by the scan
-                var username string
-                var department string
-                var age int
-                err = rows.Scan(&username, &department, &age)  // Actually read form the dataset
-                checkErr(err)
-                fmt.Println(username)
-                fmt.Println(department)
-                fmt.Println(age)
-        }
 }
 
 func main() {
-        ConnectDB()
+	ConnectDB()
 }
